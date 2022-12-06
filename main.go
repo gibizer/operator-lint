2ffa25b7f1c13fb2bdae5444a3dd1b5bbad529b1
Balazs Gibizer <gibi@redhat.com>
2022-12-06 10:16:25 +0100

    Re-enable C003

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gibizer/operator-lint/linters/crd/C001"
 	"github.com/gibizer/operator-lint/linters/crd/C002"
+	"github.com/gibizer/operator-lint/linters/crd/C003"
 	"github.com/gibizer/operator-lint/linters/envtest/T001"
 	"golang.org/x/tools/go/analysis/unitchecker"
 )
@@ -12,5 +13,6 @@ func main() {
 		T001.NewAnalyzer(),
 		C001.NewAnalyzer(),
 		C002.NewAnalyzer(),
+		C003.NewAnalyzer(),
 	)
 }
